e2c68d783cf0bc66eb0060f35482409c22790e35
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-03-03 06:39:14 +0300

    write pseudocode in JS

---
+++ Assets/develop.js
@@ -0,0 +1,43 @@
+/*
+
+GIVEN I am taking a code quiz
+
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+
+document.querySelect(".start")
+
+var timer = 0;
+
+function start() {
+  start timer i++,
+
+}
+
+WHEN I answer a question
+THEN I am presented with another question
+
+var questions = [[What's JS?], [What's concat?], [What's +=?], [What does === means?]]
+var answers = 
+
+innerHTML
+
+function answer() {
+    if (answer1 || answer2 || answer3 || answer4) {
+        go to function next question
+    }
+}
+
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+
+
+
+WHEN all questions are answered or the timer reaches 0
+THEN the game = over
+
+WHEN the game = over
+THEN I can save my initials and my score
+
+
+*/
