67c4064c372ca268c2d6cb52a8709284e18fd743
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-03-05 06:39:20 +0300

    create first two pages of the quiz

--- Assets/develop.js
+++ Assets/develop.js
@@ -5,14 +5,49 @@ GIVEN I am taking a code quiz
 WHEN I click the start button
 THEN a timer starts and I am presented with a question
 
-document.querySelect(".start")
+*/
 
-var timer = 0;
+
+
+
+var startBtn = document.getElementById("start");
+var hideBtn = document.getElementById("hideBtn");
+var timer = document.getElementById("timer");
+var question = document.getElementById("question");
+var answers = document.getElementById("answers");
+var firstAnswer = document.getElementById("firstAnswer");
+var secondAnswer = document.getElementById("secondAnswer");
+var thirdAnswer = document.getElementById("thirdAnswer");
+var fourthAnswer = document.getElementById("fourthAnswer");
+
+var timerCount = 0;
+
+document.getElementById("answers").style.display = "none";
 
 function start() {
-  start timer i++,
+  // event.preventDefault();
+  // start timer i++,
+  document.getElementById("question").textContent = "In JavaScript, what is a block of code called that is used to perform a specific task?";
+  document.getElementById("question").style.fontSize = "24px";
+  document.getElementById("text").style.display = "none";
+  document.getElementById("hideBtn").style.display = "none";
+  document.getElementById("answers").style.display = "inherit";
+  document.getElementById("firstAnswer").textContent = "Function";
+  document.getElementById("secondAnswer").textContent = "Declaration";
+  document.getElementById("thirdAnswer").textContent = "String";
+  document.getElementById("fourthAnswer").textContent = "Variable";
 
+  if (firstAnswer && secondAnswer && thirdAnswer && fourthAnswer) {
+      secondquestion();
+  }
+};
+
+function secondQuestion() {
+    
 }
+
+
+/*
 
 WHEN I answer a question
 THEN I am presented with another question
