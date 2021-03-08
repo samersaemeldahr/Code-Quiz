9f645e375df03fb0a28db4ae68919871d78c8014
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-03-08 06:12:05 +0300

    Add several functions and variables

--- Assets/develop.js
+++ Assets/develop.js
@@ -15,38 +15,126 @@ var hideBtn = document.getElementById("hideBtn");
 var timer = document.getElementById("timer");
 var question = document.getElementById("question");
 var answers = document.getElementById("answers");
-var firstAnswer = document.getElementById("firstAnswer");
-var secondAnswer = document.getElementById("secondAnswer");
-var thirdAnswer = document.getElementById("thirdAnswer");
-var fourthAnswer = document.getElementById("fourthAnswer");
+var result = document.getElementById("result");
 
+var shuffledQuestions;
+var currentQuestionIndex = 0;
 var timerCount = 0;
+var timerId;
+
+
+var questions = [
+    {
+        question: "In JavaScript, what is a block of code called that is used to perform a specific task?",
+        answers: ["Function", "Boolean", "Alert", "Number"],
+        correctAnswer: "Function"
+    },
+    {
+        question: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+        answers: ["Strings", "Arrays", "Variables", "Recorders"],
+        correctAnswer: "Strings"
+    },
+    {
+        question: "In JavaScript, what is used in conjunction with HTML to “react” to certain elements?",
+        answers: ["Condition", "Events", "Boolean", "All of the above"],
+        correctAnswer: "Events"
+    },
+    {
+        question: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+        answers: ["Range", "Restriction", "Scope", "Output Level"],
+        correctAnswer: "Scope"
+    },
+];
 
 document.getElementById("answers").style.display = "none";
 
 function start() {
   // event.preventDefault();
   // start timer i++,
-  document.getElementById("question").textContent = "In JavaScript, what is a block of code called that is used to perform a specific task?";
-  document.getElementById("question").style.fontSize = "24px";
+  question.style.fontSize = "24px";
   document.getElementById("text").style.display = "none";
-  document.getElementById("hideBtn").style.display = "none";
-  document.getElementById("answers").style.display = "inherit";
-  document.getElementById("firstAnswer").textContent = "Function";
-  document.getElementById("secondAnswer").textContent = "Declaration";
-  document.getElementById("thirdAnswer").textContent = "String";
-  document.getElementById("fourthAnswer").textContent = "Variable";
+  hideBtn.style.display = "none";
+  answers.style.display = "inherit";
+  
+  nextQuestion();
+}; 
 
-  if (firstAnswer && secondAnswer && thirdAnswer && fourthAnswer) {
-      secondquestion();
-  }
-};
+var time = questions.length * 20;
 
-function secondQuestion() {
+function nextQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    question.textContent = currentQuestion.question;
+  
+    // clear out any old question choices
+    answers.innerHTML = "";
+    currentQuestion.answers.forEach(function(choice, i) {
+        // create new button for each choice
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
     
+        choiceNode.textContent = i + 1 + ". " + choice;
+    
+        // attach click event listener to each choice
+        choiceNode.onclick = questionClick;
+    
+        // display on the page
+        answers.appendChild(choiceNode);
+      });
 }
 
 
+
+function questionClick() {
+    // check if user guessed wrong
+    if (this.value !== questions[currentQuestionIndex].correctAnswer) {
+      // penalize time
+      time -= 15;
+  
+      if (time < 0) {
+        time = 0;
+      }
+      // display new time on page
+      timer.textContent = time;
+      result.textContent = "Incorrect!";
+      result.style.color = "red";
+    } else {
+      result.textContent = "Correct!";
+      result.style.color = "green";
+    }
+  
+    // flash right/wrong feedback
+    result.setAttribute("class", "feedback");
+    setTimeout(function() {
+      result.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+    // next question
+    currentQuestionIndex++;
+  
+    // time checker
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      nextQuestion();
+    }
+  }
+
+  function quizEnd() {
+    // stop timer
+    clearInterval(timerId);
+  
+    // show end screen
+    //var endScreenEl = document.getElementById("end-screen");
+    //endScreenEl.removeAttribute("class");
+  
+    // show final score
+    //var finalScoreEl = document.getElementById("final-score");
+    //finalScoreEl.textContent = time;
+  
+    // hide questions section
+    questions.setAttribute("class", "hide");
+  }
 /*
 
 WHEN I answer a question
